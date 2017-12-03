0e25b40371fed3807de6acc72c29091f1f5bf31c
Stephen <stephen@jazer.com>
2017-12-03 15:19:36 -0500

    Add files via upload

---
+++ sqs-listener.py
@@ -0,0 +1,27 @@
+import boto3
+
+# Create SQS client
+sqs = boto3.client('sqs')
+
+queue_url = 'https://sqs.us-east-1.amazonaws.com/692685737698/twitch-google-home.fifo'
+
+# Enable long polling on an existing SQS queue
+sqs.set_queue_attributes(
+    QueueUrl=queue_url,
+    Attributes={'ReceiveMessageWaitTimeSeconds': '20'}
+)
+
+# Long poll for message on provided SQS queue
+response = sqs.receive_message(
+    QueueUrl=queue_url,
+    AttributeNames=[
+        'SentTimestamp'
+    ],
+    MaxNumberOfMessages=1,
+    MessageAttributeNames=[
+        'All'
+    ],
+    WaitTimeSeconds=20
+)
+
+print(response)
