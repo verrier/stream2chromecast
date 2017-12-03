b7e4d79c6f674cf952597e2555c00782e652c0f3
Stephen <stephen@jazer.com>
2017-12-03 15:21:30 -0500

    Update sqs-listener.py

--- sqs-listener.py
+++ sqs-listener.py
@@ -1,7 +1,7 @@
 import boto3
 
 # Create SQS client
-sqs = boto3.client('sqs')
+sqs = boto3.client('sqs', region_name="us-east-1")
 
 queue_url = 'https://sqs.us-east-1.amazonaws.com/692685737698/twitch-google-home.fifo'
 
